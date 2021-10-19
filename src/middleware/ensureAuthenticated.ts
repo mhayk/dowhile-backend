d2bb350fb9c9277bf59254f86ebca5a30625bf12
Mhayk Whandson <hi@mhayk.com>
2021-10-19 22:19:07 +0100

    A middleware to append user_id in request object from express has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express"
+import { verify } from "jsonwebtoken"
+
+interface IPayload {
+    sub: string
+}
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    const authToken = request.headers.authorization
+
+    if (!authToken) {
+        return response.status(401).json({ error: "Token not provided" })
+    }
+
+    //Bearer 4732984732984329f234f923y45923
+    // [0] = Bearer
+    // [1] = 4732984732984329f234f923y45923
+    const [, token] = authToken.split(" ")
+
+    try {
+        const { sub } = verify(token, process.env.JWT_SECRET) as IPayload
+
+        request.user_id = sub
+
+        return next()
+    } catch (error) {
+        return response.status(401).json({ error: "Token invalid" })
+    }
+}
