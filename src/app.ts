26c53d4a10a666170b218f050204e9ddc2eb2c08
Mhayk Whandson <hi@mhayk.com>
2021-10-19 22:41:42 +0100

    the serverHttp and io has been exported to allow share the socket connection.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/app.ts
+++ src/app.ts
@@ -36,6 +36,4 @@ app.get("/signin/callback", (request, response) => {
     return response.json({ code })
 })
 
-serverHttp.listen(4000, () => {
-    console.log('Server is running on port 4000')
-})
+export { serverHttp, io }
