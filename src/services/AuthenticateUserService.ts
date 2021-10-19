984b22c83a3d2d06cd19d60bfb5ea242e8557e37
Mhayk Whandson <hi@mhayk.com>
2021-10-19 18:50:26 +0100

    The statement to create user via prisma has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import prismaClient from '../prisma'
+
 /**
  * Receive code(string)
  * Recover the access_token from github
@@ -14,7 +16,7 @@ interface IAccessTokenResponse {
 }
 
 interface IUserResponse {
-    avater_url: string,
+    avatar_url: string,
     login: string,
     id: number,
     name: string
@@ -40,6 +42,26 @@ class AuthenticateUserService {
                 Authorization: `Bearer ${accessTokenResponse.access_token}`
             }
         })
+
+        const { login, id, avatar_url, name } = response.data;
+
+        let user = await prismaClient.user.findFirst({
+            where: {
+                github_id: id
+            }
+        })
+
+        if (!user) {
+            user = await prismaClient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name
+                }
+            })
+        }
+
 
         return response.data
 
