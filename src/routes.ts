6f964a44ec4834a1139ed089b9569d42a6308873
Mhayk Whandson <hi@mhayk.com>
2021-10-19 23:04:42 +0100

    the endpoint '/profile' has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
 import { GetLast3MessageController } from './controllers/GetLast3MessageController'
+import { ProfileUserController } from './controllers/ProfileUserController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 import { GetLast3MessagesService } from './services/GetLast3MessagesService'
 
@@ -12,5 +13,7 @@ router.post('/authenticate', new AuthenticateUserController().handle)
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
 
 router.get('/messages/last3', new GetLast3MessageController().handle)
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
