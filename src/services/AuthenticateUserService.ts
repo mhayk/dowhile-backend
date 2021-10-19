a20b42bc6d26dc23d74031a6b19cc5881d56a30e
Mhayk Whandson <hi@mhayk.com>
2021-10-19 14:37:03 +0100

    authenticate user service has been implemented.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 /**
  * Receive code(string)
  * Recover the access_token from github
@@ -8,7 +9,20 @@
  */
 class AuthenticateUserService {
     async execute(code: string) {
+        const url = "https://github.com/login/oauth/access_token";
 
+        const response = await axios.post(url, null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code
+            },
+            headers: {
+                accept: "application/json"
+            }
+        });
+
+        return response.data;
     }
 }
 
