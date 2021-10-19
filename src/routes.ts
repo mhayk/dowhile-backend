f01775564a4bffc70a3d346ff5ee2c6b720e0b84
Mhayk Whandson <hi@mhayk.com>
2021-10-19 22:52:56 +0100

    the endpoint '/messages/last3' has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
+import { GetLast3MessageController } from './controllers/GetLast3MessageController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
+import { GetLast3MessagesService } from './services/GetLast3MessagesService'
 
 const router = Router()
 
 router.post('/authenticate', new AuthenticateUserController().handle)
 
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
+
+router.get('/messages/last3', new GetLast3MessageController().handle)
 
 export { router }
